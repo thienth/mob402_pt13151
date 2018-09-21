75b385c52ee7b80c1559df7dca09551df2075c8c
thienth32 <thienth32@gmail.com>
2018-09-21 07:32:37 +0700

    update

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : '123456',
+  database : 'mob402'
+});
+
+connection.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var keyword = req.query.keyword == undefined ? "" : req.query.keyword;
+  
+  var sql = `select * from posts `;
+  if(keyword !== ""){
+    sql += `where title like '%${keyword}%' or content like '%${keyword}%'`;
+  }
+  connection.query(sql, function (err, rows, fields) {
+    if (err) throw err;
+
+    var data = rows;
+    res.render('welcome', { data });
+  })
+
+  
+});
+
+router.get('/add-new', function(req, res, next){
+  res.render('post/add-form');
+});
+
+router.post('/add-new', function(req, res, next){
+  var insertQuery = `insert into posts 
+                      (title, image, short_desc, content, author)
+                     values (?, ?, ?, ?, ?)`;
+  var data = [
+    req.body.title,
+    req.body.image,
+    req.body.short_desc,
+    req.body.content,
+    req.body.author
+  ];
+  connection.query(insertQuery, data, function (err, rows, fields) {
+    if (err) throw err;
+
+    console.log(rows, fields);
+    res.redirect('/');
+  })
+});
+module.exports = router;

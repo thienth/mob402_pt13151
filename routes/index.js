753266fda0c34a24d8736d34a07e1e7a7208bd0d
thienth32 <thienth32@gmail.com>
2018-09-21 08:05:15 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -28,15 +28,25 @@ router.get('/', function(req, res, next) {
   
 });
 
-router.get('/remove/:id', function(req, res, next){
+router.get('/edit/:id', function(req, res, next){
   var sql = `select * from posts where id = ?`;
   var data = [req.params.id];
   connection.query(sql, data, function (err, rows, fields) {
     if (err) throw err;
 
     var data = rows[0];
-    
+
     res.render('post/post-form', { data });
+  })
+});
+
+router.get('/remove/:id', function(req, res, next){
+  var sql = `delete from posts where id = ?`;
+  var data = [req.params.id];
+  connection.query(sql, data, function (err, rows, fields) {
+    if (err) throw err;
+
+    res.redirect('/');
   })
 });
 
@@ -45,18 +55,40 @@ router.get('/add-new', function(req, res, next){
   res.render('post/post-form', { data });
 });
 
-router.post('/add-new', function(req, res, next){
-  var insertQuery = `insert into posts 
-                      (title, image, short_desc, content, author)
-                     values (?, ?, ?, ?, ?)`;
-  var data = [
-    req.body.title,
-    req.body.image,
-    req.body.short_desc,
-    req.body.content,
-    req.body.author
-  ];
-  connection.query(insertQuery, data, function (err, rows, fields) {
+router.post('/save', function(req, res, next){
+  if(req.body.id !== ""){
+    var query = ` update posts
+                  set
+                    title = ?,
+                    image = ?,
+                    short_desc = ?,
+                    content = ?,
+                    author = ?
+
+                  where id = ?`;
+    var data = [
+      req.body.title,
+      req.body.image,
+      req.body.short_desc,
+      req.body.content,
+      req.body.author,
+      req.body.id
+    ]
+  }else{
+    var query = ` insert into posts 
+                  ( title, image, short_desc, 
+                    content, author)
+                values (?, ?, ?, ?, ?)`;
+    var data = [
+      req.body.title,
+      req.body.image,
+      req.body.short_desc,
+      req.body.content,
+      req.body.author
+    ];
+  }
+  
+  connection.query(query, data, function (err, rows, fields) {
     if (err) throw err;
 
     console.log(rows, fields);
